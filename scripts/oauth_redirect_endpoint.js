646fd72087f97b8a950cc185f74125d613087466
Ítalo Silva <italosilvadesouza@gmail.com>
2018-05-17 03:35:36 -0300

    Curriculum first version created.

--- scripts/oauth_redirect_endpoint.js
+++
@@ -1,5 +0,0 @@
-function get(name){
-   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-      return decodeURIComponent(name[1]);
-}
-document.write(get('foo'))
