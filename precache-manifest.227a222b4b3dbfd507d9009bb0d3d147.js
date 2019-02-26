37c5a132da8cd7f4d63054be10673889b8b9db4e
Ítalo Silva <italosilvadesouza@gmail.com>
2019-02-26 00:42:55 -0300

    Updates

---
+++ precache-manifest.227a222b4b3dbfd507d9009bb0d3d147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe347b5210f98ff12db7",
+    "url": "/static/js/main.fe347b52.chunk.js"
+  },
+  {
+    "revision": "6435f7f75ce78e835ef7",
+    "url": "/static/js/1.6435f7f7.chunk.js"
+  },
+  {
+    "revision": "fe347b5210f98ff12db7",
+    "url": "/static/css/main.01486c22.chunk.css"
+  },
+  {
+    "revision": "6435f7f75ce78e835ef7",
+    "url": "/static/css/1.65a98fef.chunk.css"
+  },
+  {
+    "revision": "c5d58ae9fd48a757881444da60ca01be",
+    "url": "/index.html"
+  }
+];
