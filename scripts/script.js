aa398afb98a0a96b252dd16694607b20afd3375c
Ítalo Souza <italo.souza@saude.ce.gov.br>
2021-02-25 09:02:18 -0300

    Corrige calculo de idade

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 function setAge() {
   var dataNascimento = new Date(1993,1,23);
-  var diferencaEmMilisegundos = dataNascimento.getTime() - Date.now();
-  var idade = Math.abs(new Date(diferencaEmMilisegundos).getUTCFullYear() - 1970)
+  var agora = new Date();
+  var diferencaEmMilisegundos = agora.getTime() - dataNascimento.getTime();
+  var idade = Math.floor(diferencaEmMilisegundos / (1000 * 60 * 60 * 24 * 365.25));
 
   var idadeElement = document.getElementById('idade');
   var leading = idade < 10 ? '0' : ''
